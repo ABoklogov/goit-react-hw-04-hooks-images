c44bfb220be0c59beb472b53df86caf9314b2eef
Alexey Boklogov <kashira87@mail.ru>
2021-08-22 18:34:08 +0300

    corrected component App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
-import React, { Component, Fragment } from 'react';
-import { ToastContainer } from 'react-toastify';
+import React, { Fragment, useState, useEffect } from 'react';
 import Loader from 'react-loader-spinner';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import s from './App.module.css';
 import Searchbar from '../Searchbar';
 import { fetchImage, apiOptions } from '../../services/image-api';
@@ -21,42 +22,34 @@ const dataModal = {
   alt: '',
 };
 
-class App extends Component {
-  state = {
-    imageName: '',
-    images: [],
-    page: 1,
-    error: null,
-    showModal: false,
-    status: Status.IDLE,
-  };
+const App = () => {
+  const [imageName, setImageName] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [status, setStatus] = useState(Status.IDLE);
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevName = prevState.imageName;
-    const nextName = this.state.imageName;
-
-    if (prevName !== nextName) {
-      this.setState({
-        page: 1,
-        images: [],
-        status: Status.PENDING,
-      });
-
-      this.loaderImages(nextName);
+  useEffect(() => {
+    if (imageName === '') {
+      return;
     }
-  }
+    setPage(1);
+    setImages([]);
+    setStatus(Status.PENDING);
 
-  loaderImages = (name, page = 1) => {
-    this.setState({ status: Status.PENDING });
+    loaderImages(imageName);
+  }, [imageName]);
+
+  const loaderImages = (name, page = 1) => {
+    setStatus(Status.PENDING);
 
     fetchImage(name, page)
       .then(comeImages => {
         if (comeImages.total !== 0) {
-          this.setState(({ images, page }) => ({
-            images: [...images, ...comeImages.hits],
-            page: page + 1,
-            status: Status.RESOLVED,
-          }));
+          setImages(prevImages => [...prevImages, ...comeImages.hits]);
+          setPage(page + 1);
+          setStatus(Status.RESOLVED);
 
           window.scrollTo({
             top: document.documentElement.scrollHeight,
@@ -65,73 +58,63 @@ class App extends Component {
           return;
         }
 
-        return Promise.reject(new Error(`Нет такого изображения: ${name}`));
+        return Promise.reject(new Error(`No such image: ${name}`));
       })
-      .catch(error =>
-        this.setState({
-          error,
-          status: Status.REJECTED,
-        }),
-      );
+      .catch(error => {
+        setError(error);
+        setStatus(Status.REJECTED);
+      });
   };
 
-  handleFormSubmit = imageName => {
-    this.setState({ imageName });
+  const handleFormSubmit = imageName => {
+    setImageName(imageName);
   };
 
-  toggleModal = (urlImage, alt) => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  const toggleModal = (urlImage, alt) => {
+    setShowModal(showModal => !showModal);
     dataModal.url = urlImage;
     dataModal.alt = alt;
   };
 
-  render() {
-    const { images, error, status, page, imageName, showModal } = this.state;
+  return (
+    <div className={s.App}>
+      <Searchbar onSubmit={handleFormSubmit} />
 
-    return (
-      <div className={s.App}>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-
-        {status === 'pending' && (
-          <Fragment>
-            {images && (
-              <ImageGallery images={images} openModal={this.toggleModal} />
-            )}
-            <Loader
-              className={s.Loader}
-              type="ThreeDots"
-              color="#00BFFF"
-              height={100}
-              width={100}
-            />
-          </Fragment>
-        )}
-
-        {status === 'resolved' && (
-          <Fragment>
-            <ImageGallery images={images} openModal={this.toggleModal} />
-            {images.length >= apiOptions.PER_PAGE && (
-              <Button
-                loadMoreImages={() => this.loaderImages(imageName, page)}
-              />
-            )}
-          </Fragment>
-        )}
-
-        {status === 'rejected' && <ImageErrorView message={error.message} />}
-
-        {showModal && (
-          <Modal
-            bigImage={dataModal.url}
-            alt={dataModal.alt}
-            onClose={this.toggleModal}
+      {status === Status.PENDING && (
+        <Fragment>
+          {images && <ImageGallery images={images} openModal={toggleModal} />}
+          <Loader
+            className={s.Loader}
+            type="ThreeDots"
+            color="#00BFFF"
+            height={100}
+            width={100}
           />
-        )}
+        </Fragment>
+      )}
 
-        <ToastContainer />
-      </div>
-    );
-  }
-}
+      {status === Status.RESOLVED && (
+        <Fragment>
+          <ImageGallery images={images} openModal={toggleModal} />
+          {images.length >= apiOptions.PER_PAGE && (
+            <Button loadMoreImages={() => loaderImages(imageName, page)} />
+          )}
+        </Fragment>
+      )}
+
+      {status === Status.REJECTED && <ImageErrorView message={error.message} />}
+
+      {showModal && (
+        <Modal
+          bigImage={dataModal.url}
+          alt={dataModal.alt}
+          onClose={toggleModal}
+        />
+      )}
+
+      <ToastContainer />
+    </div>
+  );
+};
 
 export default App;
